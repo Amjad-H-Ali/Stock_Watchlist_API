1c293a6e55dad733c02156f6f87e60076b27596d
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-05-31 11:31:43 -0500

    Created Get Route for User Wacthed Stocks in StockController

--- controllers/StockController.rb
+++ controllers/StockController.rb
@@ -1,4 +1,35 @@
 class StockController < ApplicationController 
 
+	# Filter to prevent users from accessing routes without being logged
+	before do
+
+		if !session[:logged_in]
+			halt 200, {
+				success: false,
+
+				message: "Access Denied: Please Login."
+			}.to_json
+		end	
+	end
+
+	# Routes
+
+	# Index Route in Watchlist
+	get '/' do
+		user = User.find(session[:user_id])
+
+		user_watchlist = user.stocks.map { |obj| obj[:ticker] }
+
+
+		{
+			success: true,
+			message: "Found #{user_watchlist.length} stock(s) for #{user[:email]}.",
+			stocks: user_watchlist
+		}.to_json
+
+	end	
+
+
 
 end	
+
